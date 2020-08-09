eff3ead62788d9d4a7472a02009828a3fe54eea8
misacoding <misa.huynh425@gmail.com>
2020-08-08 22:51:01 -0400

    I commit my hello javascript

---
+++ helper.js
@@ -0,0 +1,3 @@
+document.write("<h3>See I told you have you seen any difference? </h3>")
+document.write("<h4>Look it got smaller again it will always get smaller </h4>")
+document.write("Bye guys!")
